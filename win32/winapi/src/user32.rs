f7b748aaea1fc83444e4f7ae2ab29f7d93c5e7b0
Evan Martin <evan.martin@gmail.com>
2026-03-11 14:18:41 -0700

    more stubs

--- win32/winapi/src/user32.rs
+++ win32/winapi/src/user32.rs
@@ -24,7 +24,7 @@ pub fn CreateWindowExA(
     _hInstance: HINSTANCE,
     _lpParam: u32,
 ) -> HWND {
-    todo!()
+    stub!(1)
 }
 
 #[win32_derive::dllexport]
@@ -61,22 +61,22 @@ pub fn MessageBoxA(
 
 #[win32_derive::dllexport]
 pub fn RegisterClassA(_lpWndClass: u32) -> u16 {
-    todo!()
+    stub!(1)
 }
 
 #[win32_derive::dllexport]
 pub fn SetFocus(_hWnd: HWND) -> HWND {
-    todo!()
+    stub!(0)
 }
 
 #[win32_derive::dllexport]
 pub fn ShowWindow(_hWnd: HWND, _nCmdShow: u32 /* SHOW_WINDOW_CMD */) -> bool {
-    todo!()
+    stub!(true)
 }
 
 #[win32_derive::dllexport]
 pub fn UpdateWindow(_hWnd: HWND) -> bool {
-    todo!()
+    stub!(true)
 }
 
 #[win32_derive::dllexport]
@@ -101,8 +101,16 @@ pub fn PeekMessageA(
 }
 
 #[win32_derive::dllexport]
-pub fn GetSystemMetrics(_nIndex: u32 /* SYSTEM_METRICS_INDEX */) -> i32 {
-    todo!()
+pub fn GetSystemMetrics(nIndex: u32 /* SYSTEM_METRICS_INDEX */) -> i32 {
+    // These were dumped from a win2k VM running at 640x480.
+    // See exe/rust/bin/metrics.rs.
+    const METRICS: [i32; 100] = [
+        640, 480, 16, 16, 19, 1, 1, 3, 3, 16, 16, 32, 32, 32, 32, 19, 640, 433, 0, 1, 16, 16, 0, 0,
+        0, 0, 0, 0, 112, 27, 18, 18, 4, 4, 112, 27, 4, 4, 75, 75, 0, 0, 0, 5, 0, 2, 2, 160, 24, 16,
+        16, 16, 12, 15, 18, 18, 8, 160, 24, 652, 492, 648, 460, 3, 0, 0, 0, 0, 4, 4, 0, 13, 13, 0,
+        0, 1, 0, 0, 640, 480, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ];
+    METRICS[nIndex as usize]
 }
 
 #[win32_derive::dllexport]
