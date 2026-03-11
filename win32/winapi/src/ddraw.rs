9dde7352bbfa0ec51e2e322ba29ff6f6d63ef0c7
Evan Martin <evan.martin@gmail.com>
2026-03-11 15:31:23 -0700

    start on ddraw

--- win32/winapi/src/ddraw.rs
+++ win32/winapi/src/ddraw.rs
@@ -1,8 +1,65 @@
 use runtime::*;
 
-pub type HRESULT = u32;
+use crate::{ABIReturn, stub};
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[repr(u32)]
+pub enum DD {
+    OK = 0,
+    E_NOINTERFACE = 0x80004002,
+    ERR_GENERIC = 0x80004005,
+}
+
+impl Into<ABIReturn> for DD {
+    fn into(self) -> ABIReturn {
+        (self as u32).into()
+    }
+}
+
+pub const IID_IDirectDraw7: GUID = GUID((
+    0x15e65ec0,
+    0x3b9c,
+    0x11d2,
+    [0xb9, 0x2f, 0x00, 0x60, 0x97, 0x97, 0xea, 0x5b],
+));
+
+#[repr(C)]
+#[derive(PartialEq, zerocopy::FromBytes)]
+pub struct GUID(pub (u32, u16, u16, [u8; 8]));
+
+impl std::fmt::Debug for GUID {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{:08x}-{:04x}-{:04x}-{:04x}-",
+            self.0.0,
+            self.0.1,
+            self.0.2,
+            u16::from_le_bytes(self.0.3[..2].try_into().unwrap())
+        )?;
+        for b in &self.0.3[2..] {
+            write!(f, "{:02x}", b)?;
+        }
+        Ok(())
+    }
+}
 
 #[win32_derive::dllexport]
-pub fn DirectDrawCreateEx(_lpGuid: u32, _lplpDD: u32, _iid: u32, _pUnkOuter: u32) -> HRESULT {
-    todo!()
+pub fn DirectDrawCreateEx(lpGuid: u32, lplpDD: u32, iid: u32, _pUnkOuter: u32) -> DD {
+    assert!(lpGuid == 0);
+    let iid = if iid == 0 {
+        None
+    } else {
+        Some(unsafe { MACHINE.memory.read::<GUID>(iid) })
+    };
+
+    let ddraw: u32 = match iid {
+        Some(IID_IDirectDraw7) => stub!(0),
+        _ => panic!(),
+    };
+
+    unsafe {
+        MACHINE.memory.write(lplpDD, ddraw);
+    }
+    DD::OK
 }
