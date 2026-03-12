a5d5885dce229f4115bfba35f8e1ea4aa0a8961b
Evan Martin <evan.martin@gmail.com>
2026-03-11 23:17:32 -0700

    rename

--- build-native.sh
+++ build-native.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# cargo run -p fc ~/win/rs/exe/winapi/winapi.exe exe/winapi
-# cargo run -p fc ~/win/rs/exe/zig_hello/hello.exe exe/zig_hello
-cargo run -p fc ~/win/rs/deploy/archive/BasicDD.exe exe/basicdd
+# cargo run -p tc ~/win/rs/exe/winapi/winapi.exe exe/winapi
+# cargo run -p tc ~/win/rs/exe/zig_hello/hello.exe exe/zig_hello
+cargo run -p tc ~/win/rs/deploy/archive/BasicDD.exe exe/basicdd
 cargo build -p basicdd-exe "$@"
