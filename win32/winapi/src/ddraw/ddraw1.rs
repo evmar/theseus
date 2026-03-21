19ad2bf05839b1950e01cfefdbaeffff582d7171
Evan Martin <evan.martin@gmail.com>
2026-03-21 13:20:20 -0700

    IDirectDrawSurface

--- win32/winapi/src/ddraw/ddraw1.rs
+++ win32/winapi/src/ddraw/ddraw1.rs
@@ -72,7 +72,7 @@ pub mod IDirectDraw {
             .unwrap()
             .0;
         let desc2 = DDSURFACEDESC2::from_desc(&desc);
-        let surface = ddraw.create_surface(&desc2, &mut || 0);
+        let surface = ddraw.create_surface(&desc2, &mut || IDirectDrawSurface::new());
         unsafe { MACHINE.memory.write(lplpDDSurface, surface.borrow().addr) };
 
         DD::OK
@@ -196,6 +196,292 @@ pub mod IDirectDraw {
             SetCooperativeLevel: func_addr + 20,
             SetDisplayMode: func_addr + 21,
             WaitForVerticalBlank: func_addr + 22,
+        };
+        vtable
+            .write_to_prefix(unsafe { &mut MACHINE.memory.bytes[vtable_addr as usize..] })
+            .unwrap();
+        vtable_addr
+    }
+
+    pub fn new() -> u32 {
+        let addr = kernel32::state()
+            .process_heap
+            .borrow()
+            .alloc(unsafe { &mut MACHINE.memory }, 4);
+        unsafe {
+            MACHINE.memory.write(addr, vtable());
+        }
+        addr
+    }
+}
+
+pub mod IDirectDrawSurface {
+    use super::*;
+
+    #[derive(Default, zerocopy::IntoBytes, zerocopy::Immutable)]
+    #[repr(C)]
+    pub struct VTable {
+        QueryInterface: u32,
+        AddRef: u32,
+        Release: u32,
+        AddAttachedSurface: u32,
+        AddOverlayDirtyRect: u32,
+        Blt: u32,
+        BltBatch: u32,
+        BltFast: u32,
+        DeleteAttachedSurface: u32,
+        EnumAttachedSurfaces: u32,
+        EnumOverlayZOrders: u32,
+        Flip: u32,
+        GetAttachedSurface: u32,
+        GetBltStatus: u32,
+        GetCaps: u32,
+        GetClipper: u32,
+        GetColorKey: u32,
+        GetDC: u32,
+        GetFlipStatus: u32,
+        GetOverlayPosition: u32,
+        GetPalette: u32,
+        GetPixelFormat: u32,
+        GetSurfaceDesc: u32,
+        Initialize: u32,
+        IsLost: u32,
+        Lock: u32,
+        ReleaseDC: u32,
+        Restore: u32,
+        SetClipper: u32,
+        SetColorKey: u32,
+        SetOverlayPosition: u32,
+        SetPalette: u32,
+        Unlock: u32,
+        UpdateOverlay: u32,
+        UpdateOverlayDisplay: u32,
+        UpdateOverlayZOrder: u32,
+    }
+
+    #[win32_derive::dllexport]
+    pub fn QueryInterface(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn AddRef(_this: u32) -> u32 {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn Release(_this: u32) -> u32 {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn AddAttachedSurface(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn AddOverlayDirtyRect(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn Blt(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn BltBatch(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn BltFast(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn DeleteAttachedSurface(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn EnumAttachedSurfaces(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn EnumOverlayZOrders(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn Flip(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn GetAttachedSurface(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn GetBltStatus(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn GetCaps(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn GetClipper(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn GetColorKey(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn GetDC(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn GetFlipStatus(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn GetOverlayPosition(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn GetPalette(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn GetPixelFormat(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn GetSurfaceDesc(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn Initialize(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn IsLost(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn Lock(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn ReleaseDC(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn Restore(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn SetClipper(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn SetColorKey(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn SetOverlayPosition(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn SetPalette(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn Unlock(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn UpdateOverlay(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn UpdateOverlayDisplay(_this: u32) -> DD {
+        todo!()
+    }
+
+    #[win32_derive::dllexport]
+    pub fn UpdateOverlayZOrder(_this: u32) -> DD {
+        todo!()
+    }
+
+    fn vtable() -> u32 {
+        let vtable_addr = kernel32::state().process_heap.borrow().alloc(
+            unsafe { &mut MACHINE.memory },
+            std::mem::size_of::<VTable>() as u32,
+        );
+        let func_addr = runtime::proc_addr(QueryInterface_stdcall);
+        let vtable = VTable {
+            QueryInterface: func_addr + 0,
+            AddRef: func_addr + 1,
+            Release: func_addr + 2,
+            AddAttachedSurface: func_addr + 3,
+            AddOverlayDirtyRect: func_addr + 4,
+            Blt: func_addr + 5,
+            BltBatch: func_addr + 6,
+            BltFast: func_addr + 7,
+            DeleteAttachedSurface: func_addr + 8,
+            EnumAttachedSurfaces: func_addr + 9,
+            EnumOverlayZOrders: func_addr + 10,
+            Flip: func_addr + 11,
+            GetAttachedSurface: func_addr + 12,
+            GetBltStatus: func_addr + 13,
+            GetCaps: func_addr + 14,
+            GetClipper: func_addr + 15,
+            GetColorKey: func_addr + 16,
+            GetDC: func_addr + 17,
+            GetFlipStatus: func_addr + 18,
+            GetOverlayPosition: func_addr + 19,
+            GetPalette: func_addr + 20,
+            GetPixelFormat: func_addr + 21,
+            GetSurfaceDesc: func_addr + 22,
+            Initialize: func_addr + 23,
+            IsLost: func_addr + 24,
+            Lock: func_addr + 25,
+            ReleaseDC: func_addr + 26,
+            Restore: func_addr + 27,
+            SetClipper: func_addr + 28,
+            SetColorKey: func_addr + 29,
+            SetOverlayPosition: func_addr + 30,
+            SetPalette: func_addr + 31,
+            Unlock: func_addr + 32,
+            UpdateOverlay: func_addr + 33,
+            UpdateOverlayDisplay: func_addr + 34,
+            UpdateOverlayZOrder: func_addr + 35,
         };
         vtable
             .write_to_prefix(unsafe { &mut MACHINE.memory.bytes[vtable_addr as usize..] })
