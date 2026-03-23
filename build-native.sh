58da85d5930bc6fc1dee8900f38f7cb0b8f5facd
Evan Martin <evan.martin@gmail.com>
2026-03-22 21:27:57 -0700

    start importing thread

--- build-native.sh
+++ build-native.sh
@@ -16,6 +16,10 @@ elif [[ $1 == "chillin" ]]; then
         --out exe/chillin \
         --extern 0x40a3b4
     cargo run --release -p chillin-exe
+elif [[ $1 == "thread" ]]; then
+    cargo run -p tc -- --exe ~/win/rs/exe/cpp/thread.exe --out exe/thread
+    cargo build -p thread-exe
+    ./target/debug/thread-exe
 else
     cargo run -p tc -- --scan --exe ~/win/rs/deploy/archive/BasicDD.exe --out exe/basicdd
     cargo build -p basicdd-exe
