66db7d51b1993dd7c56c9f4896fec527c0066914
Evan Martin <evan.martin@gmail.com>
2026-03-27 07:51:28 -0700

    chillin passes timer proc on stack

--- build-native.sh
+++ build-native.sh
@@ -14,7 +14,8 @@ elif [[ $1 == "chillin" ]]; then
     cargo run -p tc -- \
         --exe ~/win/rs/scratch/demo/chillin-unpacked.exe \
         --out exe/chillin \
-        --extern 0x40a3b4
+        --extern 0x40a3b4 \
+        --scan-immediates
     cargo run --release -p chillin-exe
 elif [[ $1 == "thread" ]]; then
     cargo run -p tc -- --exe ~/win/rs/exe/cpp/thread.exe --out exe/thread --scan-immediates
