ceaf7b951e0c6cd204b3492346a7e013e3186326
Evan Martin <evan.martin@gmail.com>
2026-03-27 17:12:20 -0700

    move last op handlers out

--- tc/src/codegen/mod.rs
+++ tc/src/codegen/mod.rs
@@ -1,6 +1,8 @@
 use anyhow::{Result, anyhow, bail};
 mod control_flow;
 mod fpu;
+mod math;
+mod misc;
 mod mmx;
 mod string;
 
@@ -194,219 +196,14 @@ fn gen_block(w: &mut Writer, state: &State, ip: AddrAbs, block: &Block) {
 fn gen_instr(w: &mut Writer, state: &State, instr: &iced_x86::Instruction) {
     log::info!("gen: {}", instr);
     w.line(format!("// {:08x} {}", AddrAbs(instr.ip32()).0, instr));
-    use iced_x86::Mnemonic::*;
-    match instr.mnemonic() {
-        Push => w.line(format!("push(ctx, {});", get_op(instr, 0))),
-        Pop => {
-            w.line("let x = pop(ctx);");
-            w.line(set_op(instr, 0, "x".into()))
-        }
-        Pushad => w.line("pushad(ctx);"),
-        Popad => w.line("popad(ctx);"),
-        Mov => w.line(set_op(instr, 0, get_op(instr, 1))),
-
-        // Binary operations.
-        And | Or | Add | Sub | Sbb | Xor | Shl | Shr | Sar => {
-            assert_eq!(instr.op_count(), 2);
-            let func = instr_name(instr);
-            let op0 = get_op(instr, 0);
-            let op1 = get_op(instr, 1);
-            w.line(set_op(
-                instr,
-                0,
-                format!("{func}({op0}, {op1}, &mut ctx.cpu.flags)"),
-            ));
-        }
-
-        Adc => {
-            assert_eq!(instr.op_count(), 2);
-            let op0 = get_op(instr, 0);
-            let op1 = get_op(instr, 1);
-            w.line("let carry = ctx.cpu.flags.contains(Flags::CF) as u32;");
-            w.line(set_op(
-                instr,
-                0,
-                format!("addc({op0}, {op1}, carry as _, &mut ctx.cpu.flags)"),
-            ));
-        }
-
-        Shld => {
-            assert_eq!(instr.op_count(), 3);
-            let op0 = get_op(instr, 0);
-            let op1 = get_op(instr, 1);
-            let op2 = get_op(instr, 2);
-            w.line(set_op(
-                instr,
-                0,
-                format!("shld({op0}, {op1}, {op2}, &mut ctx.cpu.flags)"),
-            ));
-        }
-        Shrd => w.todo(),
-
-        Cmp => {
-            let op0 = get_op(instr, 0);
-            let op1 = get_op(instr, 1);
-            w.line(format!("sub({op0}, {op1}, &mut ctx.cpu.flags);"));
-        }
-        Test => {
-            w.line(format!(
-                "and({}, {}, &mut ctx.cpu.flags);",
-                get_op(instr, 0),
-                get_op(instr, 1)
-            ));
-        }
-
-        Lea => w.line(format!("{} = {};", get_op(instr, 0), gen_addr(instr))),
-        Neg => w.line(set_op(
-            instr,
-            0,
-            format!("neg({}, &mut ctx.cpu.flags)", get_op(instr, 0)),
-        )),
-
-        Movzx => {
-            w.line(set_op(instr, 0, format!("{} as _", get_op(instr, 1))));
-        }
-        Movsx => {
-            let read = format!(
-                "{read} as i{src} as i{dst} as u{dst}",
-                read = get_op(instr, 1),
-                src = op_size(instr, 1),
-                dst = op_size(instr, 0)
-            );
-            w.line(set_op(instr, 0, read));
-        }
-
-        Mul => {
-            assert_eq!(instr.op_count(), 1);
-            match op_size(instr, 0) {
-                32 => {
-                    w.line("let x = ctx.cpu.regs.eax;");
-                    w.line(format!("let y = {};", get_op(instr, 0)));
-                    w.line("let out = mul(x as u64, y as u64, &mut ctx.cpu.flags);");
-                    w.line("ctx.cpu.regs.edx = (out >> 32) as u32;");
-                    w.line("ctx.cpu.regs.eax = out as u32;");
-                }
-                16 => w.todo(),
-                8 => w.todo(),
-                size => todo!("{size}"),
-            }
-        }
-
-        Leave => w.line("leave(ctx);"),
-        Enter => {
-            assert!(instr.op1_kind() == iced_x86::OpKind::Immediate8_2nd);
-            let op1 = instr.immediate8_2nd();
-            w.line(format!("enter(ctx, {}, {:x});", get_op(instr, 0), op1));
-        }
-
-        Dec => w.line(set_op(
-            instr,
-            0,
-            format!("dec({}, &mut ctx.cpu.flags)", get_op(instr, 0)),
-        )),
-        Inc => w.line(set_op(
-            instr,
-            0,
-            format!("inc({}, &mut ctx.cpu.flags)", get_op(instr, 0)),
-        )),
-        Sete => w.line(set_op(instr, 0, "sete(ctx)".into())),
-
-        Imul => {
-            let size = op_size(instr, 0);
-            let (x, y) = match instr.op_count() {
-                1 => match op_size(instr, 0) {
-                    8 => (get_reg(iced_x86::Register::AL), get_op(instr, 0)),
-                    16 => (get_reg(iced_x86::Register::AX), get_op(instr, 0)),
-                    _ => todo!(),
-                },
-                2 => {
-                    assert_eq!(op_size(instr, 0), op_size(instr, 1));
-                    let op0 = get_op(instr, 0);
-                    let op1 = get_op(instr, 1);
-                    (op0, op1)
-                }
-                3 => {
-                    assert_eq!(op_size(instr, 0), op_size(instr, 1));
-                    assert_eq!(op_size(instr, 1), op_size(instr, 2));
-                    let op1 = get_op(instr, 1);
-                    let op2 = get_op(instr, 2);
-                    (op1, op2)
-                }
-                _ => todo!(),
-            };
-            w.line(format!("let x = {x} as i{size}; let y = {y} as i{size};",));
-            w.line(
-                "let (res, overflow) = x.overflowing_mul(y);
-                    ctx.cpu.flags.set(Flags::CF, overflow);
-                    ctx.cpu.flags.set(Flags::OF, overflow);",
-            );
-            w.line(set_op(instr, 0, format!("res as u{size}")));
-        }
-
-        Cdq => {
-            w.line("let t = ctx.cpu.regs.eax as i32 as i64 as u64;");
-            w.line("ctx.cpu.regs.edx = (t >> 32) as u32;");
-            w.line("ctx.cpu.regs.eax = t as u32;");
-        }
-
-        Idiv => {
-            assert_eq!(instr.op_count(), 1);
-            match op_size(instr, 0) {
-                8 => {
-                    w.line(
-                            "let x = (((ctx.cpu.regs.get_dl() as u16) << 8) | (ctx.cpu.regs.get_al() as u16)) as i16;",
-                        );
-                    w.line(format!("let y = {} as i16;", get_op(instr, 0)));
-                    w.line("ctx.cpu.regs.set_al((x / y) as i8 as u8);");
-                    w.line("ctx.cpu.regs.set_dl((x % y) as i8 as u8);");
-                }
-                16 => {
-                    w.line(
-                            "let x = (((ctx.cpu.regs.get_dx() as u32) << 16) | (ctx.cpu.regs.get_ax() as u32)) as i32;",
-                        );
-                    w.line(format!("let y = {} as i32;", get_op(instr, 0)));
-                    w.line("ctx.cpu.regs.set_ax((x / y) as i16 as u16);");
-                    w.line("ctx.cpu.regs.set_dx((x % y) as i16 as u16);");
-                }
-                32 => {
-                    w.line(
-                            "let x = (((ctx.cpu.regs.edx as u64) << 32) | (ctx.cpu.regs.eax as u64)) as i64;",
-                        );
-                    w.line(format!("let y = {} as i64;", get_op(instr, 0)));
-                    w.line("ctx.cpu.regs.eax = (x / y) as i32 as u32;");
-                    w.line("ctx.cpu.regs.edx = (x % y) as i32 as u32;");
-                }
-                _ => todo!(),
-            }
-        }
-
-        Xchg => {
-            w.line(format!("let t = {};", get_op(instr, 0)));
-            w.line(set_op(instr, 0, get_op(instr, 1)));
-            w.line(set_op(instr, 1, "t".into()));
-        }
-        Nop => {}
-
-        Not | Int | Int3 | Cmpxchg | Pushfd | Setne | Cpuid | Xgetbv | Setg | Bt | Div | Setge => {
-            w.todo()
-        }
-
-        Cbw => w.line("ctx.cpu.regs.set_ax(ctx.cpu.regs.get_al() as i8 as i16 as u16);"),
-        Cwde => w.line("ctx.cpu.regs.eax = ctx.cpu.regs.get_ax() as i16 as i32 as u32;"),
-
-        Stc | Clc | Std | Cld | Sahf => {
-            w.line(format!("{}(ctx);", instr_name(instr)));
-        }
-
-        c => {
-            if control_flow::codegen(w, state, instr) {
-            } else if fpu::codegen(w, state, instr) {
-            } else if string::codegen(w, state, instr) {
-            } else if mmx::codegen(w, state, instr) {
-            } else {
-                todo!("{:?} in {}", c, instr);
-            }
-        }
+    if control_flow::codegen(w, state, instr) {
+    } else if math::codegen(w, state, instr) {
+    } else if string::codegen(w, state, instr) {
+    } else if misc::codegen(w, state, instr) {
+    } else if fpu::codegen(w, state, instr) {
+    } else if mmx::codegen(w, state, instr) {
+    } else {
+        todo!("{:?} in {}", instr.mnemonic(), instr);
     }
 }
 
