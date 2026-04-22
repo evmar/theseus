6ae8b1d4f02d8a7e26c4fc246fdd72b952151db4
Evan Martin <evan.martin@gmail.com>
2026-04-22 11:36:19 -0700

    convert to library

---
+++ tc/src/lib.rs
@@ -0,0 +1,82 @@
+use std::collections::HashMap;
+
+use crate::memory::Memory;
+
+mod codegen;
+mod load;
+pub use load::load_pe;
+mod memory;
+mod traverse;
+pub use traverse::Traverse;
+
+pub struct Import {
+    pub dll: String,
+    pub func: String,
+    /// address to write func_addr to
+    pub iat_addr: u32,
+    /// address of code
+    pub func_addr: u32,
+}
+
+#[derive(Default)]
+pub struct State {
+    pub image_base: u32,
+    pub entry_point: u32,
+    pub mem: Memory,
+    pub code_memory: std::ops::Range<u32>,
+    pub imports: HashMap<u32, Import>,
+    pub blocks: HashMap<u32, Block>,
+    pub resources: Option<(u32, u32)>,
+}
+
+pub fn is_abs_memory_ref(instr: &iced_x86::Instruction) -> Option<u32> {
+    let iced_x86::OpKind::Memory = instr.op0_kind() else {
+        return None;
+    };
+    let iced_x86::Register::None = instr.memory_base() else {
+        return None;
+    };
+    let iced_x86::Register::None = instr.memory_index() else {
+        return None;
+    };
+    Some(instr.memory_displacement32())
+}
+
+pub enum Block {
+    Instrs(Vec<iced_x86::Instruction>),
+    Stdcall(String),
+    Extern(u32),
+}
+
+impl Block {
+    pub fn name(&self) -> String {
+        match self {
+            Block::Instrs(instrs) => format!("x{:08x}", instrs[0].ip32()),
+            Block::Stdcall(func) => format!("{}_stdcall", func),
+            Block::Extern(ip) => format!("x{:08x}", ip),
+        }
+    }
+}
+
+pub fn write_if_changed(path: &str, contents: &[u8]) -> anyhow::Result<()> {
+    let existing = std::fs::read(&path).unwrap_or_default();
+    if existing != contents {
+        std::fs::write(path, contents)?;
+    }
+    Ok(())
+}
+
+pub fn generate(state: &mut State, outdir: &str) -> anyhow::Result<()> {
+    codegen::gen_file(state, outdir)?;
+
+    let data_dir = format!("{outdir}/data");
+    std::fs::create_dir_all(&data_dir)?;
+    for map in state.mem.mappings.vec().iter() {
+        let buf = state.mem.slice(map.addr, map.size);
+        if buf.iter().all(|&b| b == 0) {
+            continue;
+        }
+        write_if_changed(&format!("{outdir}/data/{:08x}.raw", map.addr), buf)?;
+    }
+    Ok(())
+}
--- tc/src/main.rs
+++ tc/src/main.rs
@@ -1,64 +1,3 @@
-mod codegen;
-mod load;
-mod memory;
-mod traverse;
-
-use std::collections::HashMap;
-
-use anyhow::Result;
-use memory::*;
-
-use crate::{load::load_pe, traverse::Traverse};
-
-struct Import {
-    dll: String,
-    func: String,
-    /// address to write func_addr to
-    iat_addr: u32,
-    /// address of code
-    func_addr: u32,
-}
-
-#[derive(Default)]
-pub struct State {
-    image_base: u32,
-    entry_point: u32,
-    mem: Memory,
-    code_memory: std::ops::Range<u32>,
-    imports: HashMap<u32, Import>,
-    blocks: HashMap<u32, Block>,
-    resources: Option<(u32, u32)>,
-}
-
-pub fn is_abs_memory_ref(instr: &iced_x86::Instruction) -> Option<u32> {
-    let iced_x86::OpKind::Memory = instr.op0_kind() else {
-        return None;
-    };
-    let iced_x86::Register::None = instr.memory_base() else {
-        return None;
-    };
-    let iced_x86::Register::None = instr.memory_index() else {
-        return None;
-    };
-    Some(instr.memory_displacement32())
-}
-
-enum Block {
-    Instrs(Vec<iced_x86::Instruction>),
-    Stdcall(String),
-    Extern(u32),
-}
-
-impl Block {
-    pub fn name(&self) -> String {
-        match self {
-            Block::Instrs(instrs) => format!("x{:08x}", instrs[0].ip32()),
-            Block::Stdcall(func) => format!("{}_stdcall", func),
-            Block::Extern(ip) => format!("x{:08x}", ip),
-        }
-    }
-}
-
 fn hex(val: &str) -> Result<u32, String> {
     if !val.starts_with("0x") {
         return Err("hex value must start with 0x".into());
@@ -90,19 +29,11 @@ struct Args {
     externs: Vec<u32>,
 }
 
-pub fn write_if_changed(path: &str, contents: &[u8]) -> Result<()> {
-    let existing = std::fs::read(&path).unwrap_or_default();
-    if existing != contents {
-        std::fs::write(path, contents)?;
-    }
-    Ok(())
-}
-
-fn run() -> Result<()> {
+fn run() -> anyhow::Result<()> {
     logger::init();
     let args: Args = argh::from_env();
 
-    let mut state = State::default();
+    let mut state = tc::State::default();
     state
         .mem
         .mappings
@@ -110,32 +41,20 @@ fn run() -> Result<()> {
 
     for addr in args.externs {
         log::info!("extern: {addr:#x}");
-        state.blocks.insert(addr, Block::Extern(addr));
+        state.blocks.insert(addr, tc::Block::Extern(addr));
     }
 
     let buf = std::fs::read(args.exe).unwrap();
-    load_pe(&mut state, buf);
+    tc::load_pe(&mut state, buf);
 
     let start = state.entry_point;
-    let mut traverse = Traverse::new(&mut state, args.scan_immediates, start);
+    let mut traverse = tc::Traverse::new(&mut state, args.scan_immediates, start);
     if args.scan {
         traverse.scan_for_pointers();
     }
     traverse.run();
 
-    let outdir = &args.out;
-    codegen::gen_file(&mut state, outdir)?;
-
-    let data_dir = format!("{outdir}/data");
-    std::fs::create_dir_all(&data_dir)?;
-    for map in state.mem.mappings.vec().iter() {
-        let buf = state.mem.slice(map.addr, map.size);
-        if buf.iter().all(|&b| b == 0) {
-            continue;
-        }
-        write_if_changed(&format!("{outdir}/data/{:08x}.raw", map.addr), buf)?;
-    }
-    Ok(())
+    tc::generate(&mut state, &args.out)
 }
 
 fn main() {
