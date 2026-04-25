3d17cc326fb00f708d5dcb2e361d1a6d14c22f9e
Evan Martin <evan.martin@gmail.com>
2026-04-24 22:27:16 -0700

    restructure

--- tc/src/traverse.rs
+++ tc/src/traverse.rs
@@ -1,20 +1,26 @@
 //! Instruction stream traversal, scanning for basic blocks.
 
-use std::collections::{HashSet, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 
-use crate::{Block, State, is_abs_memory_ref};
+use crate::{Block, Module, State, is_abs_memory_ref, memory::Memory};
 
 pub struct Traverse<'a> {
-    state: &'a mut State,
+    module: &'a Module,
+    mem: &'a Memory,
+    iat_refs: &'a HashMap<u32, String>,
     scan_immediates: bool,
     queue: VecDeque<u32>,
     invalid: HashSet<u32>,
+    blocks: &'a mut HashMap<u32, Block>,
 }
 
 impl<'a> Traverse<'a> {
     pub fn new(state: &'a mut State, scan_immediates: bool, start: u32) -> Traverse<'a> {
         let mut traverse = Traverse {
-            state,
+            module: &state.module,
+            blocks: &mut state.blocks,
+            iat_refs: &state.iat_refs,
+            mem: &state.mem,
             scan_immediates,
             queue: VecDeque::new(),
             invalid: HashSet::new(),
@@ -29,12 +35,12 @@ impl<'a> Traverse<'a> {
 
     pub fn run(&mut self) {
         while let Some(ip) = self.queue.pop_front() {
-            if self.state.blocks.contains_key(&ip) || self.invalid.contains(&ip) {
+            if self.blocks.contains_key(&ip) || self.invalid.contains(&ip) {
                 continue;
             }
             match self.decode_one(ip) {
                 Ok(block) => {
-                    self.state.blocks.insert(ip, block);
+                    self.blocks.insert(ip, block);
                 }
                 Err(e) => {
                     log::warn!("omitting {ip:08x}: {e}");
@@ -45,7 +51,7 @@ impl<'a> Traverse<'a> {
     }
 
     fn decode_one(&mut self, ip: u32) -> anyhow::Result<Block> {
-        let data = self.state.mem.slice_all(ip);
+        let data = self.mem.slice_all(ip);
         if data[..0x10].iter().all(|&b| b == 0) {
             anyhow::bail!("block appears zero-filled");
         }
@@ -61,9 +67,9 @@ impl<'a> Traverse<'a> {
                     if instr.op_kind(i) == iced_x86::OpKind::Immediate32 {
                         let imm = instr.immediate32();
                         if imm == 0x401bff {
-                            log::info!("found, {:x?}", self.state.module.code_memory);
+                            log::info!("found, {:x?}", self.module.code_memory);
                         }
-                        if self.state.module.code_memory.contains(&imm) {
+                        if self.module.code_memory.contains(&imm) {
                             log::info!("{imm:x} looks like a code pointer");
                             Self::enqueue(&mut self.queue, imm);
                         }
@@ -89,7 +95,7 @@ impl<'a> Traverse<'a> {
                         }
                         iced_x86::OpKind::Memory => {
                             if let Some(addr) = is_abs_memory_ref(&instr) {
-                                if self.state.iat_refs.contains_key(&addr) {
+                                if self.iat_refs.contains_key(&addr) {
                                     // ok
                                 } else {
                                     log::warn!("{ip:08x} {instr}  ; indirect via memory");
@@ -122,19 +128,18 @@ impl<'a> Traverse<'a> {
     }
 
     pub fn scan_for_pointers(&mut self) {
-        for i in 0..self.state.mem.mappings.vec().len() {
-            let mapping = &self.state.mem.mappings.vec()[i];
-            if mapping.addr == 0 || mapping.addr == self.state.module.code_memory.start {
+        for i in 0..self.mem.mappings.vec().len() {
+            let mapping = &self.mem.mappings.vec()[i];
+            if mapping.addr == 0 || mapping.addr == self.module.code_memory.start {
                 continue;
             }
             log::info!("scanning mapping {:?}", mapping);
             let mapping_addr = mapping.addr;
-            let data =
-                self.state.mem.bytes[mapping.addr as usize..][..mapping.size as usize].to_vec();
+            let data = self.mem.bytes[mapping.addr as usize..][..mapping.size as usize].to_vec();
             for ofs in 0..data.len() - 4 {
                 let value =
                     u32::from_le_bytes([data[ofs], data[ofs + 1], data[ofs + 2], data[ofs + 3]]);
-                if self.state.module.code_memory.contains(&value) {
+                if self.module.code_memory.contains(&value) {
                     log::info!(
                         "{addr:08x}: found possible code pointer {value:x}",
                         addr = mapping_addr + ofs as u32
