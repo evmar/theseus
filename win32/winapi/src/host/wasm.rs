5bedc6e580ea9e0c3e636fb8428b7fdec0c7264d
Evan Martin <evan.martin@gmail.com>
2026-05-20 19:12:10 -0700

    wasm window, surface

--- win32/winapi/src/host/wasm.rs
+++ win32/winapi/src/host/wasm.rs
@@ -4,11 +4,42 @@ use web_sys::wasm_bindgen::prelude::*;
 
 use crate::{RECT, host};
 
-pub struct Surface {}
+pub struct Surface {
+    canvas: web_sys::HtmlCanvasElement,
+    width: u32,
+    context: web_sys::CanvasRenderingContext2d,
+}
 
 impl Surface {
+    pub fn new(width: u32, height: u32) -> Self {
+        let document = web_sys::window().unwrap().document().unwrap();
+        let canvas = document
+            .create_element("canvas")
+            .unwrap()
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .unwrap();
+        canvas.set_width(width);
+        canvas.set_height(height);
+        let context = canvas
+            .get_context("2d")
+            .unwrap()
+            .unwrap()
+            .dyn_into::<web_sys::CanvasRenderingContext2d>()
+            .unwrap();
+        Self {
+            canvas,
+            width,
+            context,
+        }
+    }
+
     pub fn set_pixels(&mut self, pixels: &[u8], stride: u32) {
-        todo!()
+        let image_data = web_sys::ImageData::new_with_u8_clamped_array(
+            wasm_bindgen::Clamped(pixels),
+            self.width,
+        )
+        .unwrap();
+        self.context.put_image_data(&image_data, 0.0, 0.0).unwrap();
     }
 
     pub fn copy(&mut self, window: &mut Window, dst_rect: &RECT, src: &Surface, src_rect: &RECT) {
@@ -16,17 +47,58 @@ impl Surface {
     }
 }
 
-pub struct Window {}
+pub struct Window {
+    dom: web_sys::HtmlCanvasElement,
+    context: web_sys::CanvasRenderingContext2d,
+}
 
 impl Window {
+    pub fn new(title: &str, width: u32, height: u32) -> Self {
+        let document = web_sys::window().unwrap().document().unwrap();
+        let dom = document
+            .create_element("canvas")
+            .unwrap()
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .unwrap();
+        let context = dom
+            .get_context("2d")
+            .unwrap()
+            .unwrap()
+            .dyn_into::<web_sys::CanvasRenderingContext2d>()
+            .unwrap();
+        dom.set_class_name("window");
+        let mut window = Self { dom, context };
+        window.resize(width, height);
+        window
+    }
+
     pub fn create_surface(&mut self, width: u32, height: u32) -> Surface {
-        todo!()
+        let surface = Surface::new(width, height);
+        self.dom.append_child(&surface.canvas).unwrap();
+        surface
     }
+
     pub fn resize(&mut self, width: u32, height: u32) {
-        todo!()
+        let style = self.dom.style();
+        style
+            .set_property("width", &format!("{}px", width))
+            .unwrap();
+        style
+            .set_property("height", &format!("{}px", height))
+            .unwrap();
+        self.context = self
+            .dom
+            .get_context("2d")
+            .unwrap()
+            .unwrap()
+            .dyn_into::<web_sys::CanvasRenderingContext2d>()
+            .unwrap();
     }
+
     pub fn render(&mut self, surface: &mut Surface) {
-        todo!()
+        self.context
+            .draw_image_with_html_canvas_element(&surface.canvas, 0.0, 0.0)
+            .unwrap();
     }
 }
 
@@ -78,7 +150,7 @@ impl Host {
     }
 
     pub fn create_window(&self, title: &str, width: u32, height: u32) -> Window {
-        todo!()
+        Window::new(title, width, height)
     }
 
     pub fn create_audio_stream(&self, spec: host::AudioSpec) -> AudioStream {
