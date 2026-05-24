ee73ba1d15c83689337121b9b1c389ae3c1a7d58
Evan Martin <evan.martin@gmail.com>
2026-05-24 07:40:57 -0700

    better wasm flags

--- build-wasm.sh
+++ build-wasm.sh
@@ -2,10 +2,22 @@
 
 set -e
 
-#exports="-Clink-args=--export=__heap_base -Clink-args=--export=__wasm_init_tls -Clink-args=--export=__tls_size -Clink-args=--export=__tls_align -Clink-args=--export=__tls_base"
-#RUSTFLAGS="-C target-feature=+atomics,+bulk-memory -C link-arg=--shared-memory -C link-arg=--import-memory  $exports"
+# For wasm output we need to pass a bunch of flags to Rust.
+# Linker flags end up looking like `-Clink-args=--XXX`, and these two loops accumulate the prefixes.
 
-export RUSTFLAGS='-Ctarget-feature=+atomics -Clink-args=--shared-memory -Clink-args=--max-memory=1073741824 -Clink-args=--import-memory -Clink-args=--export=__heap_base -Clink-args=--export=__wasm_init_tls -Clink-args=--export=__tls_size -Clink-args=--export=__tls_align -Clink-args=--export=__tls_base'
+link_flags="--shared-memory --max-memory=1073741824 --import-memory --export=__heap_base --export=__wasm_init_tls --export=__tls_size --export=__tls_align --export=__tls_base"
+link_args=""
+for arg in $link_flags; do
+    link_args="$link_args link-args=$arg"
+done
+
+RUSTFLAGS=""
+for arg in target-feature=+atomics $link_args; do
+    RUSTFLAGS="$RUSTFLAGS -C$arg"
+done
+
+echo "RUSTFLAGS=$RUSTFLAGS"
+export RUSTFLAGS
 cargo +nightly build -Z build-std=std,panic_abort --target wasm32-unknown-unknown -p winapi-exe
 wasm-bindgen --out-dir web --typescript --target web --reference-types \
     target/wasm32-unknown-unknown/debug/winapi_wasm.wasm
