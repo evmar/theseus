8e194aba1ace7abb2682c1c39f81c4fa5d26e4ca
Evan Martin <evan.martin@gmail.com>
2026-05-28 09:17:03 -0700

    simpler

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -27,7 +27,7 @@ pub const RETURN_FROM_X86_ADDR: u32 = 0xffff_fffe;
 
 impl Context {
     /// Call an x86 stdcall function, only returning once the function returns.
-    pub fn call32_x86(&mut self, mut f: Cont, args: Vec<u32>) {
+    pub fn call32_x86(&mut self, f: Cont, args: Vec<u32>) {
         let esp = self.cpu.regs.esp;
         for arg in args.into_iter().rev() {
             self.push32(arg);
@@ -37,12 +37,7 @@ impl Context {
         // esp and we abort the loop before invoking the continuation.
         self.push32(RETURN_FROM_X86_ADDR);
 
-        let mut i = 0;
-        while self.cpu.regs.esp != esp {
-            self.recent[i] = f.0;
-            i = (i + 1) % self.recent.len();
-            f = f.0(self);
-        }
+        self.cpu_loop(f, esp);
     }
 
     pub fn cpu_loop(&mut self, mut f: Cont, target_esp: u32) {
