514091855e1232997c5fb19f7a989c1b854db5b4
Evan Martin <evan.martin@gmail.com>
2026-08-20 22:58:32 -0700

    table header

--- tc/src/gather/report.rs
+++ tc/src/gather/report.rs
@@ -9,9 +9,14 @@ pub struct Report {
     pub iat: Vec<IATEntry>,
 }
 
-fn table<'a>(rows: impl Iterator<Item = Vec<String>>) -> String {
+fn table<'a>(header: &[&str], rows: impl Iterator<Item = Vec<String>>) -> String {
     let mut out = String::new();
     out.push_str("<table>");
+    out.push_str("<tr>");
+    for cell in header {
+        out.push_str(&format!("<th>{cell}</th>"));
+    }
+    out.push_str("</tr>");
     for row in rows {
         out.push_str("<tr>");
         for cell in row {
@@ -31,10 +36,14 @@ impl Report {
             "body { font-family: system-ui, sans-serif; font-size: 14px; font-variant-numeric: tabular-nums;",
             " margin: 3em 4em; }",
             "h1, h2 { font-weight: normal; }",
+            "table {
+                text-align: left;
+            }",
         ]
         .join("\n");
         let title = format!("<h1>Theseus analysis of {name}</h1>", name = self.name);
         let html_iat = table(
+            &["addr", "func"],
             self.iat
                 .into_iter()
                 .map(|IATEntry { addr, func }| vec![format!("{:x}", addr), func]),
